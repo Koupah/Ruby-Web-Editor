a87f791c6797e1d7a793e8987df334c5bdf55e19
Koupah <ktayl21@yahoo.com.au>
2021-04-10 02:48:27 +1000

    Added basic Program creation

---
+++ classes/Program.rb
@@ -0,0 +1,22 @@
+class Program < Display
+  def initialize(screen, header)
+    @screen = screen
+
+    super(0, 8, screen.width, screen.height - header.height - 1);
+    setColors(Curses::COLOR_CYAN, Curses::COLOR_BLACK, 2)
+
+    setBorder("|", "-")
+    setColors(Curses::COLOR_GREEN, Curses::COLOR_BLACK, 3)
+    setCursor(1, 1)
+    addText "#{@display.curx()} + #{@display.cury()} #{screen.height}"
+  end
+
+  def start
+
+    # TODO
+    # Logic handling input
+    # Navigate to and from settings
+
+    @display.getch
+  end
+end
