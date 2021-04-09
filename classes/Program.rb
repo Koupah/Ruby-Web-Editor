d639016a0da129b35ce7deda84053e7119a844d7
Koupah <ktayl21@yahoo.com.au>
2021-04-10 05:19:15 +1000

    Added option selections

--- classes/Program.rb
+++ classes/Program.rb
@@ -1,14 +1,32 @@
+require_relative "display/Debug"
+
 class Program < Display
-  def initialize(screen, header)
-    @screen = screen, @header = header;
+  def initialize(screen, header, arguments)
+    super(0, header.height + 1, arguments[:debug] ? screen.width / 2 : screen.width, screen.height - header.height - 1)
 
-    super(0, 8, screen.width, screen.height - header.height - 1);
+    @screen = screen, @header = header, @arguments = arguments
+
+    if arguments[:debug]
+      @debug = Debug.new((screen.width / 2) + 1, header.height + 1, (screen.width / 2) - 1, screen.height - header.height - 1)
+      @debug.setup()
+
+      debug "@debug created"
+    end
+
+    debug "Arguments: #{arguments}"
+
+    # Color our border Cyan
     setColors(Curses::COLOR_CYAN, Curses::COLOR_BLACK, 2)
-
     setBorder("|", "-")
+
+    # We want content to be green
     setColors(Curses::COLOR_GREEN, Curses::COLOR_BLACK, 3)
+
+    # Set our cursor inside of the box for writing content
     setCursor(1, 1)
-    addText("#{@display.curx()} + #{@display.cury()} #{screen.height}")
+
+    debug("#{makeSelection("Choose an option!", [{text: "option 1", value: 1}, {text: "option 2", value: 2}, {text: "third option", value: 3}])}");
+
   end
 
   def start
@@ -18,5 +36,12 @@ class Program < Display
     # Navigate to and from settings
 
     @display.getch
+  end
+
+  # Debug method to clean up the calling of printing debug
+  def debug(output)
+    if @debug != nil
+      @debug.debug(output)
+    end
   end
 end
--- classes/display/Displayable.rb
+++ classes/display/Displayable.rb
@@ -2,39 +2,45 @@ require "curses"
 
 module Displayable
 
+  include Curses;
+
+  def update
+    doupdate;
+  end
+
   def setColors(col1, col2, id)
     Curses.init_pair(id, col1, col2)
     @display.color_set(id)
-    @display.refresh
+    @display.refresh();
   end
 
-  def setCursor(x, y) 
-    @display.setpos(y, x);
+  def setCursor(x, y)
+    @display.setpos(y, x)
   end
 
   def getCursor
-    return [@display.curx(), @display.cury];
+    return [@display.curx(), @display.cury]
   end
 
   def clearDisplay
-    @display.clear;
+    @display.clear
   end
 
   def setBorder(vertical, horizontal)
     @display.box(vertical, horizontal)
-    @display.refresh;
+    @display.refresh();
   end
 
   def setText(text)
     @display.clear
     @display.addstr(text)
-    @display.refresh
+    @display.refresh();
   end
 
   def seperator(character)
     @display.setpos(@height, 0)
     @display.addstr(character * @width)
-    @display.refresh
+    update()
   end
 
   def addText(text)
@@ -45,5 +51,64 @@ module Displayable
   def fillLine(text)
     @display.addstr text * @width
     @display.refresh
+  end
+
+  def clearBox()
+    setCursor(1, 1)
+    setColors(Curses::COLOR_BLACK, Curses::COLOR_BLACK, 100)
+    (1..(@height - 1)).each { |y| setCursor(1, y); addText(" " * (@width - 2)) }
+  end
+
+  def setKeypad(bool)
+    @display.keypad = bool
+    if (bool)
+      cbreak
+      noecho
+    else
+      nocbreak
+      echo
+    end
+  end
+
+  def makeSelection(message, selections)
+    selection = 0
+    max = selections.length - 1
+    setKeypad(true)
+    
+    while true
+      clearBox()
+      @display.color_set(2)
+      setCursor(1, 1)
+
+      addText(message)
+
+      current = 0
+      selections.each { |hash|
+        setCursor(1, 3 + current)
+        @display.color_set(current == selection ? 3 : 2)
+        addText((current == selection ? "-> " : "   ") + hash[:text])
+        current += 1
+      }
+
+      character = @display.getch
+
+      case character
+      when KEY_UP
+        selection = [selection -= 1, 0].max
+      when "s", "S", KEY_DOWN
+        selection = [selection += 1, max].min
+      when 10
+        break;
+      else
+        debug "#{character}";
+      end
+
+      debug "done"
+
+      doupdate
+    end
+    setKeypad(false)
+
+    return selections[selection][:value];
   end
 end
