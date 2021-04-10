f349d764dafa84c995e00a28ae600cc4ee806f93
Koupah <ktayl21@yahoo.com.au>
2021-04-11 06:02:36 +1000

    Added title and other small changes

--- classes/Program.rb
+++ classes/Program.rb
@@ -1,14 +1,15 @@
 require_relative "display/Debug"
 require_relative "features/ConfigManager"
 require_relative "features/ConfigDeleter"
-require_relative "display/Color"
+require_relative "features/ConfigSharer"
+require_relative "features/ConfigApplier"
 
 class Program < Display
   def initialize(screen, header, arguments)
     super(0, header.height + 1, arguments[:debug] ? screen.width / 2 : screen.width, screen.height - header.height - 1)
 
     @screen = screen, @header = header, @arguments = arguments
-    
+
     if arguments[:debug]
       @debug = Debug.new((screen.width / 2) + 1, header.height + 1, (screen.width / 2) - 1, screen.height - header.height - 1)
       @debug.setup()
@@ -25,26 +26,21 @@ class Program < Display
 
     # TODO
     # DONEish - Logic handling input
-    # NEED TO DO - Navigate to and from settings
+    # DONE - NEED TO DO - Navigate to and from settings
     # DONE - Color our border Cyan
-    Color.set(self, :cyan)
-    setBorder("|", "-")
-
-    setCursor(5, 0)
-    addText("OPTIONS")
-
-    # We want content to be green
-    Color.set(self, :green, :bright)
-
-    # Set our cursor inside of the box for writing content
-    setCursor(1, 1)
 
     while true
-      case getSelectionInput("What would you like to do?", [{ text: "Create/Edit Web Editor Config", value: 1 }, { text: "Export/Import Web Editor Config", value: 2 }, { text: "Delete Web Editor Config", value: 3 }, { text: "Apply/Use Web Editor Config", value: 4 }, { text: "Exit Program", value: 9 }])
+      defaultBox(self, "Ruby Web Editor - Home Screen")
+
+      case getSelectionInput("What would you like to do?", [{ text: "Create/Edit Config", value: 1 }, { text: "Import/Export Config", value: 2 }, { text: "Delete a Config", value: 3 }, { text: "Apply/Use Web Editor Config", value: 4 }, { text: "Exit Program", value: 9 }])
       when 1
         ConfigManager.start(self)
+      when 2
+        ConfigSharer.start(self)
       when 3
         ConfigDeleter.start(self)
+      when 4
+        ConfigApplier.start(self)
       when 9
         break
       end
